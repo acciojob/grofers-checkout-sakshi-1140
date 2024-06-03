94b15e52a4d262c857a8f03e5ec6b09e86d67f46
Sakshi Choudhary <103510144+sakshi-1140@users.noreply.github.com>
2024-06-03 20:01:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,26 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const prices=document.querySelectorAll(".price");
+	let totalPrice=0
+	
+	prices.forEach((item)=> totalPrice+=Number(item.textContent))
+	// remove total 
+	const total=document.getElementById("total")
+    if(total) total.remove();
+	//add total
+	const table= document.querySelector("table")
+	const row=document.createElement("tr")
+    row.setAttribute("id", "total");
+	row.innerHTML=`
+	        <td class="item">Total</td>
+	      <td class="prices" id="ans">${totalPrice}</td>
+	      `
+	   table.append(row)
+
   
 };
 
 getSumBtn.addEventListener("click", getSum);
 
+       ==
